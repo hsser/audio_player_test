1dfd157430f1866aa3ebfdc9df0871ae144bb20c
Sisi Huang <hsspig@gmail.com>
2024-10-24 09:52:26 -0700

    Remove unused audio background functionality and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:audio_player/my_audio_player.dart';
-import 'package:just_audio_background/just_audio_background.dart';
 
-void main() async {
-  await JustAudioBackground.init(
-    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
-    androidNotificationChannelName: 'Audio playback',
-    androidNotificationOngoing: true,
-  );
+void main() {
   runApp(const MyApp());
 }
 
